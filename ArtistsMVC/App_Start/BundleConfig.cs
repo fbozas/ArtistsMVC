2cf2cd4aadee381b4e7a1ffb7f802623278794ac
Panos Bozas <fbozas@hotmail.com>
2022-06-06 20:44:05 +0300

    Changed the theme of MVC Starter

--- ArtistsMVC/App_Start/BundleConfig.cs
+++ ArtistsMVC/App_Start/BundleConfig.cs
@@ -1,5 +1,4 @@
-﻿using System.Web;
-using System.Web.Optimization;
+﻿using System.Web.Optimization;
 
 namespace ArtistsMVC
 {
@@ -23,7 +22,7 @@ namespace ArtistsMVC
                       "~/Scripts/bootstrap.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
+                      "~/Content/bootstrap-simplex.css",
                       "~/Content/site.css"));
         }
     }
