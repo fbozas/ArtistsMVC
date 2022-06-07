9e954303586eab64bc90adbef9cf30c6c88f3bc9
Panos Bozas <fbozas@hotmail.com>
2022-06-07 19:19:38 +0300

    Added  SongsController and ApplicationDbContext on it

---
+++ ArtistsMVC/Controllers/SongsController.cs
@@ -0,0 +1,24 @@
+﻿using ArtistsMVC.Models;
+using System.Web.Mvc;
+
+namespace ArtistsMVC.Controllers
+{
+    public class SongsController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+        public SongsController()
+        {
+            _context = new ApplicationDbContext();
+        }
+        // GET: Songs
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            _context.Dispose();
+        }
+    }
+}
