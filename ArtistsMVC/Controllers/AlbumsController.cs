0407239abf8fe006f9b152cb4fea6c4e69e13b33
Panos Bozas <fbozas@hotmail.com>
2022-06-10 18:16:36 +0300

    Added Artist on Delete view of Album

--- ArtistsMVC/Controllers/AlbumsController.cs
+++ ArtistsMVC/Controllers/AlbumsController.cs
@@ -101,7 +101,11 @@ namespace ArtistsMVC.Controllers
             {
                 return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
             }
-            Album album = db.Albums.Find(id);
+
+            Album album = db.Albums
+                .Include(a => a.Artist)
+                .SingleOrDefault(a => a.ID == id);
+
             if (album == null)
             {
                 return HttpNotFound();
