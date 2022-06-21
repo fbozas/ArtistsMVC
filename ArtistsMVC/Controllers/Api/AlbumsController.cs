79c340d8dbe2c88edb447e4b2ed00d6df7a0e0fe
Panos Bozas <fbozas@hotmail.com>
2022-06-21 18:51:18 +0300

    Added a web api controller with camel notation settings

---
+++ ArtistsMVC/Controllers/Api/AlbumsController.cs
@@ -0,0 +1,35 @@
+﻿using ArtistsMVC.Models;
+using ArtistsMVC.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace ArtistsMVC.Controllers.Api
+{
+    public class AlbumsController : ApiController
+    {
+        private readonly AlbumRepository _albumRepository;
+
+        public AlbumsController()
+        {
+            _albumRepository = new AlbumRepository();
+        }
+        
+        public IEnumerable<Album> GetAlbums()
+        {
+            return _albumRepository.GetAll();
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                _albumRepository.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
