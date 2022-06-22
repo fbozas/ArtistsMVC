d7bd7771bec629b66ee6c5d0e1d28507d6838284
Panos Bozas <fbozas@hotmail.com>
2022-06-22 21:04:11 +0300

    Delete feature (completed): Added bootbox

--- ArtistsMVC/App_Start/BundleConfig.cs
+++ ArtistsMVC/App_Start/BundleConfig.cs
@@ -19,7 +19,8 @@ namespace ArtistsMVC
                         "~/Scripts/modernizr-*"));
 
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/bootstrap.js"));
+                      "~/Scripts/bootstrap.js",
+                      "~/Scripts/bootbox.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap-simplex.css",
