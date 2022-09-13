4dce65b467c9810cb4d691b0770072e3bbd8096b
Panos Bozas <fbozas@hotmail.com>
2022-09-13 20:36:59 +0300

    Roles Management: Added Controller with roles lits

---
+++ ArtistsMVC/Controllers/CustomRoleManagementController.cs
@@ -0,0 +1,34 @@
+﻿using ArtistsMVC.Models;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ArtistsMVC.Controllers
+{
+    public class CustomRoleManagementController : Controller
+    {
+        private ApplicationDbContext _context;
+        private RoleManager<IdentityRole> _roleManager;
+
+        public CustomRoleManagementController()
+        {
+            _context = new ApplicationDbContext();
+            _roleManager = new RoleManager<IdentityRole>(new RoleStore<IdentityRole>(_context));
+        }
+
+        public ActionResult Index()
+        {
+            return View(_roleManager.Roles.ToList());
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            _context.Dispose();
+        }
+
+    }
+}
